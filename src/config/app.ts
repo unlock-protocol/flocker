11cfd15391491308de8f6d8791e3d7f593bafd5c
Julien Genestoux <julien.genestoux@gmail.com>
2022-11-17 22:44:58 -0500

    default to polygon

--- src/config/app.ts
+++ src/config/app.ts
@@ -3,5 +3,5 @@ export const app = {
   recaptchaKey: process.env.NEXT_PUBLIC_RECAPTCHA_KEY!,
   unlockApp: process.env.NEXT_PUBLIC_UNLOCK_APP!,
   baseURL: process.env.NEXT_PUBLIC_BASE_URL!,
-  defaultNetwork: Number(process.env.NEXT_PUBLIC_DEFAULT_NETWORK) || 100,
+  defaultNetwork: Number(process.env.NEXT_PUBLIC_DEFAULT_NETWORK) || 137,
 };
