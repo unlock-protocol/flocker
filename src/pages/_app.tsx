d88a62f0985f0077e58a8fc994b6149929e252ea
Julien Genestoux <julien.genestoux@gmail.com>
2022-11-22 11:30:46 -0500

    adding hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,9 @@ import { wagmiClient } from "../config/wagmi";
 import { Toaster } from "react-hot-toast";
 import { DefaultSeo } from "next-seo";
 import { DEFAULT_SEO } from "../config/seo";
+import { hotjar } from 'react-hotjar';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,7 +28,23 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
+
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!hotjar.initialized()) {
+      hotjar.initialize(3259412, 6);
+    }
+  }, [])
+
+  useEffect(() => {
+    if (hotjar.initialized()) {
+      hotjar.stateChange(router.asPath)
+    }
+  }, [router.asPath]);
+
   return (
     <QueryClientProvider client={queryClient}>
       <WagmiConfig client={wagmiClient}>
